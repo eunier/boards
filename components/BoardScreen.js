6c2d6771a711e16525739f4b93cd255828b0320d
Yunier Alvarez <yunieralvarez@protonmail.com>
2019-07-19 02:42:23 -0400

    WIP - Add Firebase

--- components/BoardScreen.js
+++ components/BoardScreen.js
@@ -10,8 +10,47 @@ import { List, ListItem, Button, Icon } from 'react-native-elements';
 import firebase from '../Firebase';
 
 class BoardScreen extends Component {
-  static navigationOptions = {
-    title: 'Board List'
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: 'Board List',
+      headerRight: (
+        <Button
+          buttonStyle={{ padding: 0, backgroundColor: 'transparent' }}
+          icon={{ name: 'add-circle', style: { marginRight: 0, fontSize: 28 } }}
+          onPress={() => {
+            navigation.push('AddBoard');
+          }}
+        />
+      )
+    };
+  };
+
+  constructor() {
+    super();
+    this.ref = firebase.firestore().collection('boards');
+    this.unsubscribe = null;
+    this.state = {
+      isLoading: true,
+      boards: []
+    };
+  }
+
+  onCollectionUpdate = querySnapshot => {
+    const boards = [];
+    querySnapshot.forEach(doc => {
+      const { title, description, author } = doc.data();
+      boards.push({
+        key: doc.id,
+        doc, // DocumentSnapshot
+        title,
+        description,
+        author
+      });
+    });
+    this.setState({
+      boards,
+      isLoading: false
+    });
   };
 
   render() {
